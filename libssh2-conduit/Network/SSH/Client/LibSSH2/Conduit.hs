4c32c261b279e1ca47526fb648181c2318a2bfd7
Ilya V. Portnov <portnov@iportnov.ru>
2012-02-01 12:27:51 +0600

    First version of libssh2-conduit.

---
+++ libssh2-conduit/Network/SSH/Client/LibSSH2/Conduit.hs
@@ -0,0 +1,73 @@
+
+module Network.SSH.Client.LibSSH2.Conduit where
+
+import Control.Monad
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Resource
+import Control.Concurrent.STM
+import Data.Conduit
+import qualified Data.ByteString as B
+
+import Network.SSH.Client.LibSSH2.Foreign
+import Network.SSH.Client.LibSSH2
+
+sourceChannel :: Channel -> Source IO String
+sourceChannel ch = src
+  where
+    src = Source pull close
+
+    pull = do
+        (sz, res) <- liftIO $ readChannel ch 0x400
+        if sz > 0
+            then return $ Open src res
+            else return Closed
+
+    close = return ()
+
+splitLines :: Resource m => Conduit String m String
+splitLines = conduitState "" push close
+  where
+    push st input =
+      case lines input of
+        [] -> return $ StateProducing "" [st]
+        [s1,s2] -> return $ StateProducing s2 [s1]
+        (h:t) -> return $ StateProducing (last t) ((st ++ h): init t)
+
+    close st = return [st]
+
+execCommand :: Session -> String -> Source IO String
+execCommand s cmd = execCommandS s cmd $= splitLines
+
+execCommandS :: Session -> String -> Source IO String
+execCommandS s command =
+  Source {
+      sourcePull = do
+        (key, ch) <- withIO start cleanup
+        pull key ch
+    , sourceClose = return () }
+  where
+    start = do
+      ch <- openChannelSession s
+      return ch
+    
+    next key ch =
+      Source (pullAnswer key ch) (release key)
+
+    pullAnswer key ch = do
+      (sz, res) <- liftIO $ readChannel ch 0x400
+      if sz > 0
+        then return $ Open (next key ch) res
+        else return Closed
+
+    pull key ch = do
+      liftIO $ channelExecute ch command
+      pullAnswer key ch
+
+    cleanup ch = do
+      closeChannel ch
+      exitStatus <- channelExitStatus ch
+      closeChannel ch
+      freeChannel ch
+      return ()
+
---
+++ libssh2-conduit/ssh-client.hs
@@ -0,0 +1,31 @@
+
+import Control.Monad
+import Control.Monad.Trans.Resource
+import Data.Conduit
+import Data.Conduit.Lazy
+import System.Environment
+import System.FilePath
+import Codec.Binary.UTF8.String
+
+import Network.SSH.Client.LibSSH2.Foreign
+import Network.SSH.Client.LibSSH2.Conduit
+import Network.SSH.Client.LibSSH2
+
+main = do
+  args <- getArgs
+  case args of
+    [user, host, port, cmd]  -> ssh user host (read port) cmd
+    _ -> putStrLn "Synopsis: ssh-client USERNAME HOSTNAME PORT COMMAND"
+
+ssh login host port command = do
+  initialize True
+  home <- getEnv "HOME"
+  let known_hosts = home </> ".ssh" </> "known_hosts"
+      public = home </> ".ssh" </> "id_rsa.pub"
+      private = home </> ".ssh" </> "id_rsa"
+  withSessionBlocking host port $ \session -> do
+    r <- checkHost session host port known_hosts
+    publicKeyAuthFile session login public private ""
+    res <- runResourceT $ lazyConsume $ execCommand session command
+    forM (map decodeString res) putStrLn
+  exit
